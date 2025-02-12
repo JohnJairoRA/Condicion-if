307f1838d84eb7c9cfda3fff4e07dc7678868bfe
JohnJairoRA <dragonnegrojj@gmail.com>
2025-02-11 22:54:44 -0500

    Añade el ejemplo de como es el uso de if en javascritp

---
+++ app.js
@@ -0,0 +1,7 @@
+let NumeroSecreto = 6;
+let NumeroDeUsuario = prompt("Me indicas un numero por favor:");
+console.log(NumeroDeUsuario);
+
+if (NumeroDeUsuario == NumeroSecreto){
+    alert('Adivinaste el numero');
+}
